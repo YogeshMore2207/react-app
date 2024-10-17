09dfdee605603701f9c324dcd08a88310e5a94cc
YogeshMore2207 <102284703+YogeshMore2207@users.noreply.github.com>
2024-10-18 02:26:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello World</h2>
-      </header>
+    <div>
+      <p>hello world</p>
     </div>
   );
 }
