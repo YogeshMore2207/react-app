72c6c0fb6c50803775f69addc48c7561fad1393a
YogeshMore2207 <102284703+YogeshMore2207@users.noreply.github.com>
2024-10-18 02:17:42 +0530

    new

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello World</h2>
-        <p>
-          Welcome All Buddies to Buddy Webinar!!!
-        </p>
       </header>
     </div>
   );
